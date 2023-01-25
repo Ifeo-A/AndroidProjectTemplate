698bcdf753c867624854234f92682a353f14628b
Flashspeed <Flashspeed@users.noreply.github.com>
2023-01-25 22:48:24 +0000

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,105 @@
+import java.lang.Integer.parseInt
+
+@Suppress("DSL_SCOPE_VIOLATION") // Remove when fixed https://youtrack.jetbrains.com/issue/KTIJ-19369
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.kapt)
+    alias(libs.plugins.ksp)
+}
+
+android {
+    namespace = "com.ife.android_project_template"
+    compileSdk = parseInt(myProjectConfig.versions.compileSdk.get())
+
+    defaultConfig {
+        applicationId = myProjectConfig.versions.appId.get()
+        minSdk = parseInt(myProjectConfig.versions.minSdk.get())
+        targetSdk = parseInt(myProjectConfig.versions.targetSdk.get())
+        versionCode = parseInt(myProjectConfig.versions.versionCode.get())
+        versionName = myProjectConfig.versions.versionName.get()
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility(JavaVersion.VERSION_1_8)
+        targetCompatibility(JavaVersion.VERSION_1_8)
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = libs.versions.androidxComposeCompiler.get()
+    }
+    packagingOptions {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime)
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.compose.ui)
+    implementation(libs.compose.ui.tooling.preview)
+    androidTestImplementation(libs.compose.ui.test.junit4)
+    androidTestImplementation (libs.androidx.test.ext.junit)
+    implementation(libs.androidx.compose.material)
+
+    // Arch Components
+    implementation(libs.androidx.hilt.navigation.compose)
+    androidTestImplementation(libs.androidx.test.runner)
+    androidTestImplementation(libs.androidx.hilt.navigation.compose)
+
+    // Coroutines
+    implementation(libs.kotlinx.coroutines.android)
+    testImplementation(libs.kotlinx.coroutines.test)
+
+    // Hilt Dependency Injection
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.compiler)
+    // Hilt and instrumented tests.
+    androidTestImplementation(libs.hilt.android.testing)
+    kaptAndroidTest(libs.hilt.android.compiler)
+    // Hilt and Robolectric tests.
+    testImplementation(libs.hilt.android.testing)
+    androidTestImplementation(libs.hilt.android.testing)
+    androidTestAnnotationProcessor(libs.hilt.android.compiler)
+    kaptTest(libs.hilt.android.compiler)
+
+    //Retrofit
+    implementation(libs.retrofit)
+    implementation(libs.retrofit.converter.moshi)
+
+    //Moshi
+    implementation(libs.moshi.kotlin)
+    ksp(libs.moshi.codegen)
+
+    //Logging
+    implementation(libs.timber)
+
+    debugImplementation(libs.compose.ui.tooling)
+    debugImplementation(libs.compose.ui.test.manifest)
+
+    // Junit testing
+    testImplementation(libs.junit)
+
+    // Hilt and instrumented tests.
+    androidTestImplementation(libs.mockk.android)
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,21 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+    versionCatalogs {
+        create("myProjectConfig") {
+            from(files("gradle/myproject.config.toml"))
+        }
+    }
+}
+rootProject.name = "AndroidProjectTemplate"
+include(":app")
